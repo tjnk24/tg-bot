3b0e86fb23d7d12f1acef38c21344b11e1cdf6d5
tjnk24 <tjnk24@gmail.com>
2020-09-10 03:34:18 +0300

    playing around with telegraf methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import http from 'http';
 import cors from 'cors';
 import express from 'express';
+import dotenv from 'dotenv';
+import Telegraf from 'telegraf';
+import scrape from './scrape';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('zdarova!'));
+
+bot.help((ctx) => ctx.reply('Send me a fuckin sticker'));
+
+bot.on('sticker', (ctx) => ctx.reply('sdfsdfsdfsdf'));
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.launch();
 
 const app = express();
 
@@ -9,7 +26,9 @@ app.server = http.createServer(app);
 app.use(cors({}));
 
 app.get('/', (req, res) => {
-  res.send('Hello W!');
+  scrape().then((response) => {
+    res.send(response.data);
+  });
 });
 
 app.server.listen(process.env.PORT || 8080, () => {
